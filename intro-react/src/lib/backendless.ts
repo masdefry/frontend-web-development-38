49f99698cf46d1dc93ac69071ef6504ae2a62c73
masdefry <muhammaddefryan@gmail.com>
2026-02-25 14:05:11 +0700

    day08 done

---
+++ intro-react/src/lib/backendless.ts
@@ -0,0 +1,6 @@
+import Backendless from 'backendless';
+
+var APP_ID = 'FE9C7CE4-1812-45EA-89E0-E802F281EA22';
+var API_KEY = '50EC33EE-98B9-4E74-8EE0-C128863CD91D';
+
+Backendless.initApp(APP_ID, API_KEY);
--- intro-react/src/pages/carts/page.tsx
+++ intro-react/src/pages/carts/page.tsx
@@ -1,21 +1,34 @@
+import useTotalCartStore from '../../stores/useTotalCartStore';
 export default function CartPage() {
+  const { itemsCart } = useTotalCartStore();
+
   return (
     <main className='p-10'>
       <h1 className='text-2xl font-bold text-green-700'>Cart Item</h1>
       <section className='border border-gray-300 mt-3 rounded-md p-10'>
-        <div className='flex gap-3 justify-between'>
-          <div className='flex gap-3'>
-            <div className='w-20 h-20 bg-gray-200 rounded-md'></div>
-            <div>
-              <h2 className='font-bold'>Product Name</h2>
-              <h2 className='text-sm text-gray-500'>Rp.xxx</h2>
+        {itemsCart?.map((item, index) => {
+          return (
+            <div className='flex gap-3 justify-between'>
+              <div className='flex gap-3'>
+                <div className='w-20 h-20 bg-gray-200 rounded-md'></div>
+                <div>
+                  <h2 className='font-bold'>{item?.name}</h2>
+                  <h2 className='text-sm text-gray-500'>
+                    Rp.{item?.price?.toLocaleString('id-ID')}
+                  </h2>
+                </div>
+              </div>
+              <div>
+                <h2 className='text-sm font-bold text-gray-600'>
+                  x{item?.quantity}
+                </h2>
+                <h2 className='text-sm font-bold text-gray-600'>
+                  Rp.{(item?.price * item?.quantity).toLocaleString('id-ID')}
+                </h2>
+              </div>
             </div>
-          </div>
-          <div>
-            <h2 className='text-sm font-bold text-gray-600'>x10</h2>
-            <h2 className='text-sm font-bold text-gray-600'>Rp.xxx</h2>
-          </div>
-        </div>
+          );
+        })}
       </section>
     </main>
   );
